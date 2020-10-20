62ee2e25f17092980a7ad7ff1b009361bf613082
JumaKahiga <kabirumwangi@gmail.com>
2020-10-20 19:01:55 +0300

    setup crawler

---
+++ cyclops/resources/crawler/spiders/cyclone_spider.py
@@ -0,0 +1,45 @@
+import logging
+
+from scrapy.http import Request
+from scrapy.spiders import Spider
+
+logger = logging.getLogger(__name__)
+
+
+class CycloneSpider(Spider):
+    """Scrap cyclone data from NOAA Satelites and Information
+    """
+    name = 'cyclone_data'
+    start_urls = [
+        'https://rammb-data.cira.colostate.edu/tc_realtime/index.asp'
+    ]
+    custom_settings = {'ROBOTSTXT_OBEY': False}
+    max_retries = 5
+
+    def parse(self, response):
+        base_url = 'https://rammb-data.cira.colostate.edu/tc_realtime/'
+        storms = response.css('div.basin_storms')
+
+        for storm in storms:
+            basin = storm.css('h3::text').get()
+
+            _storm_url = storm.css('li a::attr(href)').get()
+            storm_url = base_url + _storm_url if _storm_url else None
+
+            _img_url = storm.css('li a img::attr(src)').get()
+            img_url = base_url + _img_url if _img_url else None
+
+            _storm_identifier = storm.css('li a::text').get()
+            _storm_identifier_ = _storm_identifier.rstrip(
+            ) if _storm_identifier else None
+            storm_identifier = ' '.join(
+                _storm_identifier_.split()) if _storm_identifier_ else None
+
+            basin_data = {
+                'basin': basin,
+                'storm_url': storm_url,
+                'storm_identifier': storm_identifier,
+                'img_url': img_url
+            }
+
+            yield basin_data
---
+++ tests/spiders/test_cyclone_spider.py
@@ -0,0 +1,32 @@
+import unittest
+
+from scrapy.http import Request, TextResponse
+from scrapy.utils.test import get_crawler
+
+from cyclops.resources.crawler.spiders.cyclone_spider import CycloneSpider
+
+
+class TestCycloneSpider(unittest.TestCase):
+    """Test CycloneSpider. """
+
+    def setUp(self):
+        settings = {}
+        crawler = get_crawler(CycloneSpider, settings_dict=settings)
+        self.spider = crawler._create_spider(
+            custom_settings={'ROBOTSTXT_OBEY': False})
+
+    def test_parse_method(self):
+        headers = {'User-Agent': 'cyclone-project (+http://www.twitter.com)'}
+
+        request = Request(
+            url='https://rammb-data.cira.colostate.edu/tc_realtime/index.asp',
+            headers=headers)
+
+        response = TextResponse(url=request.url, request=request)
+        data = yield self.spider.parse(response)
+
+        item = dict(list(data).pop())
+        self.assertEqual(item['basin'], 'test')
+
+    # def test_cyclone_spider(self):
+    #     pass
