293217a08739f683c2ef50b37fdad51703168ec0
JumaKahiga <kabirumwangi@gmail.com>
2020-10-21 18:57:49 +0300

    update entrypoint

--- devops/docker/entry_point.sh
+++ devops/docker/entry_point.sh
@@ -4,7 +4,6 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 echo "Setting up the database ........."
-python -m flask db init
 python -m flask db stamp heads
 python -m flask db migrate
 python -m flask db upgrade
