bdc94de7531e41e796eb369e2624e8e473f78261
Mwangi Kabiru <kabirumwangi@gmail.com>
2020-10-30 01:45:02 +0300

    Fix nosetest (#7)
    * update entry point
    
    * update entrypoint
    
    * update tests compose
    
    * add uri
    
    * debug 3
    
    * debug 4
    
    * debug 5
    
    * debug 6
    
    * debug 7
    
    * wait for db
    
    * change docker version
    
    * wait for db
    
    * fix docker version
    
    * update wait
    
    * switch command
    
    * switch to entrypoint
    
    * code cleanup

--- devops/docker/entry_point.sh
+++ devops/docker/entry_point.sh
@@ -4,7 +4,6 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 echo "Setting up the database ........."
-python -m flask db init
 python -m flask db stamp heads
 python -m flask db migrate
 python -m flask db upgrade
@@ -12,5 +11,4 @@ python -m flask db upgrade
 echo "Database setup successful ........."
 
 
-gunicorn -w 2 -b 0.0.0.0:5000 --timeout 60 --access-logformat STRING \
---access-logfile - cyclops.unicorn:app
+gunicorn -b 0.0.0.0:5000 -w 2 --timeout 60 cyclops.unicorn:app
